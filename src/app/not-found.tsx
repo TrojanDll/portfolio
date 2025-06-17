2c69fa7ff5982e3f551a8c9d4cd9f073c0e55a0c
Timur <60787064+TrojanDll@users.noreply.github.com>
2025-06-17 18:26:52 +0300

    feat: fonts added

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotFound = () => {
+  return <h1 style={{ textAlign: "center", fontSize: "48px", marginTop: "30px" }}>404</h1>;
+};
+
+export default NotFound;
---
+++ src/app/providers.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { PropsWithChildren, useState } from "react";
+
+export function Providers({ children }: PropsWithChildren) {
+  const [client] = useState(
+    new QueryClient({
+      defaultOptions: {
+        queries: {
+          refetchOnWindowFocus: false,
+          staleTime: 1 * 1000 * 60,
+        },
+      },
+    })
+  );
+
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
